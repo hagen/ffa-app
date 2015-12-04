0f0edfabff08a361775c116be6c7b613dd01a5e9
Hagen Dittmer <hagen@forefrontanalytics.com.au>
2015-12-04 11:01:39 +1100

    Incorrect spelling of getContextBinding - should be get BindingContext

--- src/view/datasets/CreateHDB.controller.js
+++ src/view/datasets/CreateHDB.controller.js
@@ -944,7 +944,7 @@ sap.ui.define(["jquery.sap.global", "com/ffa/hpc/view/datasets/CreateController"
         }
 
         // Now we'll continue.
-        var oBinding = oItem.getContextBinding("dataset");
+        var oBinding = oItem.getBindingContext("dataset");
         return oBinding.getProperty("entity");
       };
 
