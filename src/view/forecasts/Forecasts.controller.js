7e035c7d9a9e94e6ad6ec7db4fff925bfd4f06bb
Hagen Dittmer <hagen@forefrontanalytics.com.au>
2015-12-04 13:46:04 +1100

    Added more columns to the data view, and message strips indicating columns unavailable on phone/tablet.

--- src/view/forecasts/Forecasts.controller.js
+++ src/view/forecasts/Forecasts.controller.js
@@ -843,12 +843,12 @@ sap.ui.define(["jquery.sap.global", "com/ffa/hpc/view/forecasts/Controller"],
           aResults.push({
             "Date" : com.ffa.hpc.util.DateFormatter.ddMMyyyy(oData.date), // format
             "Actual" : oData.actual,
-            "Forecast" : oData.forecast,
-            "Adjusted": oData.adjustment,
-            "Change" : oData.change,
-            "Change Percent" : oData.change_percent,
-            "Difference" : oData.diff,
-            "Difference (%)" : oData.diff_percent
+            "Forecast" : oData.forecast,,
+            "Difference b/w Actual and Forecast" : oData.diff,
+            "Difference b/w Actual and Forecast (%)" : oData.diff_percent
+            "Adjusted Forecast": oData.adjustment,
+            "Difference b/w Forecast and Adjustment" : oData.change,
+            "Difference b/w Forecast and Adjustment (%)" : oData.change_percent
           });
         }
       } catch (e) {
